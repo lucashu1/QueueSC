e95a8d1a693d64a8dd65763222238c184782468a
Milan Goyal <Milan@Milans-MacBook-Pro.local>
2018-04-07 15:55:27 -0700

    gps distance calc added in queue

--- src/queues/Queue.java
+++ src/queues/Queue.java
@@ -16,8 +16,35 @@ public class Queue {
 	private double avgWaitTime;
 	private int numUsersProcessed;
 	private Vector<QueueEntry> queueEntries;
+	private double longitude;
+	private double latitude;
+	private double radius;
 	// TODO: Google Maps location
+	public boolean inRange(double userLat, double userLong) {
+	    final int R = 6371; // Radius of the earth
+
+	    double latDistance = Math.toRadians(latitude - userLat);
+	    double lonDistance = Math.toRadians(longitude - userLong);
+	    double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+	            + Math.cos(Math.toRadians(userLat)) * Math.cos(Math.toRadians(latitude))
+	            * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+	    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+	    double distance = R * c * 1000; // convert to meters
+	    distance = distance * 3.28084; //converts meters to feet
+	    
+	      if (distance <= radius) {
+	    	  	return true;
+	      }
+	      return false;
+	}
 	
+	public void setLat(double lat) {
+		latitude = lat;
+	}
+	
+	public void setLong(double longi) {
+		longitude = longi;
+	}
 	// Return true if queue contains a user with the given email
 	public boolean hasUser(String email) {
 		for (QueueEntry qe : queueEntries) {
